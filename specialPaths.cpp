8819016eb2e6babbd765253ea1dbebb545d14aec
Rohitaas Beri <110415797+rohitaas-15@users.noreply.github.com>
2022-11-15 19:28:30 +0530

    Create specialPaths.cpp

---
+++ specialPaths.cpp
@@ -0,0 +1,233 @@
+//https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/special-path-b3ac37d0/
+
+
+/*You are given an undirected connected graph  with  nodes and  edges. Every node has a value  assigned to it.
+
+The value of a simple path between node  and  is as follows:
+
+The maximum absolute difference between the values of adjacent nodes present in the simple path.
+Find the minimum possible path value of any simple paths between start and end nodes.
+
+Input format
+
+The first line contains two space-separated integers  and  denoting the number of nodes and edges respectively.
+Next  lines contain two space-separated integers denoting edges.
+The next line contains  space-separated integers denoting node values.
+The next line contains two space-separated integers denoting the start ends.*/
+
+//rohitaas_beri
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define int ll
+#define dd long double
+#define vi vector<int> 
+#define vvi vector<vi> 
+#define vvvi vector<vvi>
+#define vpii vector<pair<int,int> > 
+#define pii pair<int,int> 
+#define vvpii vector<vpii> 
+#define UB upper_bound
+#define LB lower_bound
+#define beg(v) v.begin()
+#define en(v) v.end()
+#define pb push_back
+#define nl "\n"
+#define con continue
+#define br break
+#define ret return
+#define F first
+#define S second
+#define yes print("YES")
+#define no print("NO")
+#define rep(i,a,b) for(int i =(a);i<(b);i++)
+#define all(v) v.begin(),v.end()
+#define rev(v) reverse(all(v))
+#define maxv(v) *max_element(all(v));
+#define minv(v) *min_element(all(v));
+#define sumv(v) accumulate(all(v),0ll)
+#define print(value)  cout<<(value)<<nl
+#define print2(v1,v2) cout<<(v1)<<" "<<(v2)<<nl
+#define print3(v1,v2,v3) cout<<(v1)<<" "<<(v2)<<" "<<(v3)<<nl
+#define print4(v1,v2,v3,v4) cout<<(v1)<<" "<<(v2)<<" "<<(v3)<<" "<<(v4)<<nl
+#define printv(v)rep(it,0,v.size()){cout<<v[it]<<" ";}cout<<"\n"
+#define printvv(v)rep(it,0,v.size()){rep(it2,0,v[0].size()){cout<<v[it][it2]<<" ";}cout<<"\n";}
+#define printq(Q) auto temp = Q; while(temp.size()){ cout<<temp.front()<<endl; temp.pop();}
+
+       
+#define sz(v) ((int)v.size())
+
+#define bug(...)  __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+     #ifndef ONLINE_JUDGE 
+
+     	const char* comma = strchr (names + 1, ',');
+	    cout.write (names, comma - names) << ":" << arg1 << "|"; __f (comma + 1, args...);
+                    
+    #endif 
+}
+
+
+
+//unweighted Graph 
+
+ struct Graph{
+   
+   int n;
+   vvpii adj;
+   vi vis;
+   bool undir=true,isCycle=false;
+
+   //for trees
+   int m;
+   vvi sparse;  //Storing the 2^(j)th parent of i
+   vi depth, parent;
+   vi val;
+
+   //for Directed Graphs
+   
+   vi topo; //topological ordering
+ 
+ 
+ 
+   Graph(int N)
+   {
+     n = N;
+     m = (int)log2(n) + 2;
+     adj.resize(n+1);
+     vis.resize(n+1,0);
+     depth.resize(n+1,0);
+     parent.resize(n+1,0);
+     sparse.resize(n+1,vi(m,0));
+     val.resize(n+1,0);
+   }
+ 
+   //adding edges
+ 
+   void addEdge(int x, int y, int undir = true,int w=0)
+   {
+     this->undir=undir;
+     adj[x].pb({y,w});
+ 
+     if(undir)
+     {
+       adj[y].pb({x,w});
+     }
+   }
+ } ;
+
+Graph g(1e5+1);
+
+
+
+void solve()
+{
+   int n,m;cin>>n>>m;
+   vvi edges;
+
+   Graph temp(n);
+   g = temp;
+
+   rep(i,0,m)
+   {
+    int x,y;cin>>x>>y;
+     edges.pb({x,y});
+   } 
+
+
+   rep(i,0,n)
+   {
+    cin>>g.val[i+1];
+   }
+
+   rep(i,0,m)
+   {
+     g.addEdge(edges[i][0],edges[i][1],1,abs(g.val[edges[i][0]]-g.val[edges[i][1]]));
+   }
+   int s,e;cin>>s>>e;
+
+   set<pii> S;
+   S.insert({0,s});
+   vi ans(n+1,LLONG_MAX);
+   vi vis(n+1,0);
+
+   ans[s]=0;
+   while(S.size())
+   {
+     pii check = *(S.begin());
+     S.erase(check);
+     int node = check.S;
+     int num = check.F;
+     vis[node]=1;
+
+     for(auto i : g.adj[node])
+     {
+       if(max(num,i.S)<ans[i.F] && !vis[i.F])
+       {
+        S.erase({ans[i.F],i.F});
+        S.insert({max(num,i.S),i.F});
+        ans[i.F]= max(num,i.S);
+       }
+     }
+   }
+   print(ans[e]);
+
+
+}
+
+signed main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(0);
+    clock_t printClock = clock();
+
+    int T=1;
+    //cin>>T;
+
+   
+    while(T--) solve();
+
+    cerr<< "RUNTIME : "<<((double)clock()-printClock)/CLOCKS_PER_SEC<<"s";
+}
+
+
+
+
+/* notes:
+
+1) Dont use vector.resize() without vector.clear() .
+
+2) Range of long long is -2^63 to 2^63-1 , range of unsigned long long 
+   is 0 to 2^64-1 .
+
+3) Expected number of coin throws to get N consecutive heads is 2^(n+1) - 2 .
+
+4) Upper bound of number of divisors of a number n is n^(1/3) .
+
+5) GCD(x,y)=GCD(x-y,y) , The same applies for multiple arguments .
+
+6) To meet the constraints int the question,  we can sometimes kick out one state of our dp.
+  We can achieve that by a relatively common trick: turn the removed state into the value of the dp.
+
+
+7) If we have N elements out of which we want to choose K elements and it is allowed
+  to choose one element more than once, then number of ways are given by: nCr(n+k-1,k) .
+
+
+*/
+
+
+/* Good Questions :
+
+1 ) https://www.codechef.com/submit/SECPLAYER
+
+ */
